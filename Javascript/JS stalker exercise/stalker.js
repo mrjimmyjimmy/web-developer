2d7aa37f5740abf32c3853ed67dd294618411ea4
Jimmy <mrxujimmy@gmail.com>
2019-01-24 09:29:43 +1100

    JS stalker exercise finished

---
+++ Javascript/JS stalker exercise/stalker.js
@@ -0,0 +1,5 @@
+var firstName = prompt("enter you first name");
+var lastName = prompt("enter you last name");
+var age = prompt("enter you age");
+console.log("you full name is " + firstName + " " + lastName);
+console.log("you age is " + age);
