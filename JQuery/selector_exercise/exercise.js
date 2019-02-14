742b32d9a02cf852011146c12315013a9c9f4ce8
Jimmy <mrxujimmy@gmail.com>
2019-02-14 19:57:43 +1100

    jQuery seletor exercise

---
+++ JQuery/selector_exercise/exercise.js
@@ -0,0 +1,9 @@
+$('div').css('background', 'purple');
+$('div.hightlight').css('width', '200px');
+$('div#third').css('border', '1px solid orange');
+$('div:nth-of-type(1)').css('color', 'pink');
+
+
+// wrong answer:
+// nth-of-type(1)
+// width
