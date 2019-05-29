01b44ec28cd1fee019b02a0d1aad1849989cc439
Jimmy <mrxujimmy@gmail.com>
2019-05-29 13:15:36 +1000

    refactor app, export modules

--- webdevbootcamp/YelpCamp/app.js
+++ webdevbootcamp/YelpCamp/app.js
@@ -1,7 +1,8 @@
 var express     = require('express'),
     app         = express(),
     bodyParser  = require("body-parser"),
-    mongoose    = require('mongoose')
+    mongoose    = require('mongoose'),
+    Campground  = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
 
@@ -10,16 +11,6 @@ mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true 
 app.use(bodyParser.urlencoded({extended: true}));
 // set view engine
 app.set('view engine', 'ejs');
-
-// Schema setup
-var campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-
-var Campground = mongoose.model('Campground', campgroundSchema);
-
 
 // landing page
 app.get('/', function(req, res){
